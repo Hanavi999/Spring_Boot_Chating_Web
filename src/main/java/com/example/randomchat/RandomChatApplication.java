02b6aca544ab069ac176aa0a983087cfc5baae94
Hanavi999 <chanhyeon777@gmail.com>
2023-05-01 15:00:14 +0900

    commit

--- src/main/java/com/example/randomchat/RandomChatApplication.java
+++ src/main/java/com/example/randomchat/RandomChatApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RandomChatApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RandomChatApplication.class, args);
     }
